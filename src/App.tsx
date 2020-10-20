20c69da1e16af22b20289aed3a7a386ae4699e09
h.nakayama <wenas@0gmail.com>
2020-10-20 21:45:37 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Config from './pages/Config'
+import Index from './pages/Index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Index} exact />
+        <Route path="/config" component={Config} exact />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../templates/Dashboard';
+
+const Index: React.FC = () => {
+  return (
+    <Dashboard title="トップ2ページ">
+      <h1>Top</h1>
+    </Dashboard>
+  );
+}
+
+export default Index;
